f22c2b5f8129f788be12685f3d3081c70ae9d3d4
codemind <119723659+vvsairamsaladi@users.noreply.github.com>
2023-05-05 12:13:21 +0530

    Add files via upload

---
+++ polynomialeqinlinkedlist.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    float coeff;
+    int expo;
+    struct node *next;
+};
+typedef struct node NODE;
+NODE* insert_a_term(NODE *head, float co, int ex){
+    NODE *new_term =(NODE *)malloc(sizeof(NODE));
+    new_term->coeff=co;
+    new_term->expo=ex;
+    new_term->next=NULL;
+    
+    if(head == NULL||ex>head->expo){
+        new_term->next=head;
+        head=new_term;
+    }
+    else {
+        NODE *temp=head;
+        while(temp -> next !=NULL && temp ->next->expo>ex){
+            temp = temp -> next;
+        }
+        new_term->next=temp->next;
+        temp->next=new_term;
+    }
+    return head;
+}
+NODE* create_polynomial(){
+    NODE *head = NULL;
+    int n;
+    printf("Enter No. of terms: ");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++){
+        float co;
+        int ex;
+        printf("Enter coeff of terms %d: ",i);
+        scanf("%f",&co);
+        printf("Enter expo of terms %d: ",i);
+        scanf("%d",&ex);
+        head= insert_a_term(head,co,ex);
+    }
+    return head;
+    
+}
+
+void display_polynomial(NODE *head){
+    NODE *temp = head;
+    while(temp != NULL){
+        printf("(%.1fx^%d)",temp->coeff,temp->expo);
+        if(temp->next !=NULL){
+            printf(" * ");
+        }
+        else{
+            printf("\n");
+        }
+        temp=temp->next;
+    }
+}
+
+int main(){
+    printf("polynomial:\n");
+    NODE *poly = create_polynomial();
+    display_polynomial(poly);
+}
